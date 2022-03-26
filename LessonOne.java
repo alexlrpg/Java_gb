971943236e9bf7efc0316e26552b1296837faea6
alexlrpg <91071065+alexlrpg@users.noreply.github.com>
2022-03-26 13:25:29 +0800

    Update LessonOne.java

--- LessonOne.java
+++ LessonOne.java
@@ -1,18 +1,18 @@
 public class LessonOne {
-	public static void main (String[] args) {
+    public static void main (String[] args) {
         printThreeWords();
         checkSumSign();
         printColor();
         compareNumbers();
 	}
     
-    public static void printThreeWords() {
+    static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
     
-    public static void checkSumSign() {
+    static void checkSumSign() {
         int a = -85;
         int b = 48;
         if (a + b >= 0) {
@@ -22,20 +22,18 @@ public class LessonOne {
         }
     }
     
-    public static void printColor() {
+    static void printColor() {
         int value = 99;
         if (value <= 0) {
             System.out.println("Red");
-        }
-        if (value > 0 && value <= 100) {
+        } else if (value <= 100) {
             System.out.println("Yellow");
-        }
-        if (value > 100) {
+        } else {
             System.out.println("Green");
         }
     }
     
-    public static void compareNumbers() {
+    static void compareNumbers() {
         int a = 100;
         int b = -74;
         if (a >=b) {
